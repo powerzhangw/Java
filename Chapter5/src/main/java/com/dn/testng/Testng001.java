d813d7661974739d917aa5e5b6407e61e528bf48
power <power228@qq.com>
2019-05-20 07:17:15 +0800

    update

--- Chapter5/src/main/java/com/dn/testng/Testng001.java
+++ Chapter5/src/main/java/com/dn/testng/Testng001.java
@@ -1,4 +1,28 @@
 package com.dn.testng;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Testng001 {
+
+    //
+    @Test
+    public void testCase1(){
+        System.out.println("这是测试用例1");
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试用例2");
+    }
+    @BeforeMethod
+    public void testCase3(){
+        System.out.println("这是测试方法执行前执行");
+    }
+
+    @AfterMethod
+    public void testCase4(){
+        System.out.println("AfterMethod这是测试方法之后执行");
+    }
 }
